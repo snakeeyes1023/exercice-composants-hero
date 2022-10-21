cab0f938fb5e9ab85cc50307faf3c2e896a633a9
Jonathan Côté <55141844+snakeeyes1023@users.noreply.github.com>
2022-10-21 09:15:03 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from './page/page1/page1.component';
+import { Page2Component } from './page/page2/page2.component';
+import { EnteteComponent } from './components/entete/entete.component';
+import { RechercheComponent } from './components/recherche/recherche.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { PiedPageComponent } from './components/pied-page/pied-page.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    EnteteComponent,
+    RechercheComponent,
+    HeroComponent,
+    PiedPageComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
